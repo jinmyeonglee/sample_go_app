f5db36b10534765fa4986f4b029098c6d7749d5f
jinmyeong.lee <jinmyeong.lee@linecorp.com>
2020-07-21 15:16:22 +0900

    init commit

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	//"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.String(200, "Hello, I'm Jinmyeong!")
+	})
+	r.Run()
+}
